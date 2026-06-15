a8b2c68c2de961e59579dadf4141a5909dee8dfc
kennynguyen216 <kennedyn216@gmail.com>
2026-06-15 16:51:00 -0500

    Basic client calling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient chatClient = new OllamaApiClient(
+    new Uri("http://localhost:11434"),
+    "qwen3:8b");
+
+var response = await chatClient.GetResponseAsync(
+    "Explain Big O notation in two sentences.");
+
+Console.WriteLine(response.Text);
