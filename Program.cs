70e3fb97cf71d80636ec15227922be40d2c7ad63
kennynguyen216 <kennedyn216@gmail.com>
2026-06-16 16:01:32 -0500

    feat: working multi-agent NGMI with BigOOptimizer and EdgeCaseDestroyer
    I really did this

--- Program.cs
+++ Program.cs
@@ -1,11 +1,50 @@
 ﻿using Microsoft.Extensions.AI;
 using OllamaSharp;
+using System;
 
-IChatClient chatClient = new OllamaApiClient(
-    new Uri("http://localhost:11434"),
-    "qwen3:8b");
+// error if the model takes longer than 100 seconds to respond it times out this line fixes it
+//makes it so that you can take 5 minute to respond 
 
-var response = await chatClient.GetResponseAsync(
-    "Explain Big O notation in two sentences.");
+var httpClient = new HttpClient{ 
+    
+    BaseAddress = new Uri("http://localhost:11434"),
+    Timeout = TimeSpan.FromMinutes(10)
+    
+    };
 
-Console.WriteLine(response.Text);
+
+IChatClient chatClient = new OllamaApiClient(httpClient, "qwen3:8b");
+
+
+var history = new List<ChatMessage>();
+
+var edgeHistory =  new List<ChatMessage>();
+
+history.Add(new ChatMessage(ChatRole.System,"You are BigOOptimizer. You only analyze space and time complexity. You do not discuss anything else"));
+
+edgeHistory.Add(new ChatMessage(ChatRole.System, "You are EdgeCaseDestroyer. You only analyze the edge cases of the code given and tell where they fail. You do not discuss anything else."));
+
+
+while(true)
+{
+    
+    Console.Write("Enter solution");
+    string? userResponse = Console.ReadLine();
+    if(userResponse == null) break;
+
+    history.Add(new ChatMessage(ChatRole.User, userResponse));
+    edgeHistory.Add(new ChatMessage(ChatRole.User, userResponse));
+
+    //history.Add(new ChatMessage(ChatRole.User,"Analyze this code:\npublic int[] TwoSum(int[] nums, int target) { }"));
+
+    var response = await chatClient.GetResponseAsync(history);
+    var edgeResponse = await chatClient.GetResponseAsync(edgeHistory);
+
+
+    Console.WriteLine(response.Text);
+    Console.WriteLine(edgeResponse.Text);
+
+    edgeHistory.AddMessages(edgeResponse);
+    history.AddMessages(response);
+
+}
