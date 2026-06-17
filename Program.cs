bdcbb2d24afbff7ec4c378b2d434865642419c97
kennynguyen216 <kennedyn216@gmail.com>
2026-06-17 15:49:24 -0500

    feat: add persistent memory with memory.txt

--- Program.cs
+++ Program.cs
@@ -11,15 +11,20 @@ var httpClient = new HttpClient
 
 IChatClient ollamaClient = new OllamaApiClient(httpClient, "qwen3:8b");
 
+// memory time
+
+string pastMemory =  File.ReadAllText("memory.txt");
+
+
 // define agents here
 
 AIAgent timeAgent = ollamaClient.AsAIAgent(
-    instructions: "You analyze time and space complexity of the code snippet. Nothing else",
+    instructions: $"You analyze time and space complexity of the code snippet. Nothing else... Past analyses:\n{pastMemory}",
     tools: [AIFunctionFactory.Create(Tools.RunCode)]
 );
 
 AIAgent edgeAgent = ollamaClient.AsAIAgent(
-    instructions: "You analyze edge cases and where the code snippet may fail or glitch. Nothing else",
+    instructions: $"You analyze edge cases and where the code snippet may fail or glitch. Nothing else... Past analyses:\n{pastMemory}",
     tools: [AIFunctionFactory.Create(Tools.RunCode)]
 );
 
@@ -29,11 +34,12 @@ AIAgent edgeAgent = ollamaClient.AsAIAgent(
 while (true)
 {
     Console.WriteLine("Enter Solution");
+  
     string? userResponse = Console.ReadLine();
     if(userResponse == null) break;
 
     Console.WriteLine(await timeAgent.RunAsync(userResponse));
     Console.WriteLine(await edgeAgent.RunAsync(userResponse));
-
+    File.AppendAllText("memory.txt", $"- Analyzed: {userResponse}\n");
 
 }
