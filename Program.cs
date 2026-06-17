97df237909b452ea02283c6cb0c04b0b96a41336
kennynguyen216 <kennedyn216@gmail.com>
2026-06-17 10:47:42 -0500

    using semantic kernel but that is not the move

--- Program.cs
+++ Program.cs
@@ -1,28 +1,39 @@
 ﻿using Microsoft.Extensions.AI;
 using OllamaSharp;
-using System;
+using Microsoft.Agents.AI;
 
-// error if the model takes longer than 100 seconds to respond it times out this line fixes it
-//makes it so that you can take 5 minute to respond 
-
-var httpClient = new HttpClient{ 
-    
+var httpClient = new HttpClient
+{
     BaseAddress = new Uri("http://localhost:11434"),
     Timeout = TimeSpan.FromMinutes(10)
-    
-    };
+};
+
+IChatClient ollamaClient = new OllamaApiClient(httpClient, "qwen3:8b");
+//warns me im using ollama and its lowk foreign but im saying dont worry
+#pragma warning disable SKEXP0070
+var builder = Kernel.CreateBuilder();
+builder.Services.AddSingleton<IChatCompletionService>(ollamaClient.AsChatCompletionService());
+var kernel = builder.Build();
+#pragma warning restore SKEXP0070
 
 
-IChatClient chatClient = new OllamaApiClient(httpClient, "qwen3:8b");
+var bigOAgent = new ChatCompletionAgent
+{
+    Name = "BigOOptimizer",
+    Instructions = "Only analyze space and time complexity. Nothing else",
+    Kernel = kernel
+};
 
+var edgeAgent = new ChatCompletionAgent
+{
+    Name = "EdgeCaseDestroyer",
+    Instructions = "Only analyze edge cases of code snippet. Nothing else",
+    Kernel = kernel
+};
 
-var history = new List<ChatMessage>();
+var bigOThread = new ChatHistoryAgentThread();
 
-var edgeHistory =  new List<ChatMessage>();
-
-history.Add(new ChatMessage(ChatRole.System,"You are BigOOptimizer. You only analyze space and time complexity. You do not discuss anything else"));
-
-edgeHistory.Add(new ChatMessage(ChatRole.System, "You are EdgeCaseDestroyer. You only analyze the edge cases of the code given and tell where they fail. You do not discuss anything else."));
+var edgeThread = new ChatHistoryAgentThread();
 
 
 while(true)
@@ -32,19 +43,14 @@ while(true)
     string? userResponse = Console.ReadLine();
     if(userResponse == null) break;
 
-    history.Add(new ChatMessage(ChatRole.User, userResponse));
-    edgeHistory.Add(new ChatMessage(ChatRole.User, userResponse));
+    await foreach (var message in bigOAgent.InvokeAsync(userResponse,bigOThread))
+    {
+        Console.WriteLine(message.Message.Content);
+    }
 
-    //history.Add(new ChatMessage(ChatRole.User,"Analyze this code:\npublic int[] TwoSum(int[] nums, int target) { }"));
-
-    var response = await chatClient.GetResponseAsync(history);
-    var edgeResponse = await chatClient.GetResponseAsync(edgeHistory);
-
-
-    Console.WriteLine(response.Text);
-    Console.WriteLine(edgeResponse.Text);
-
-    edgeHistory.AddMessages(edgeResponse);
-    history.AddMessages(response);
+    await foreach (var edgeMessage in edgeAgent.InvokeAsync(userResponse, edgeThread))
+    {
+        Console.WriteLine(edgeMessage.Message.Content);
+    }
 
 }
